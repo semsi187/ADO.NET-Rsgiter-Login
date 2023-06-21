3b8768d1159fad3724267f8f40dfb68ec500155f
Kenn <115916914+semsi187@users.noreply.github.com>
2023-06-22 00:54:06 +0400

    Add project files.

---
+++ ADO.NET Rsgiter Login/RegisterPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ADO.NET_Rsgiter_Login
+{
+    /// <summary>
+    /// Interaction logic for RegisterPage.xaml
+    /// </summary>
+    public partial class RegisterPage : Window
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            if (NewPassword.Text == ReNewPassword.Text)
+            {
+                MessageBox.Show("Registered");
+                Thread.Sleep(1000);
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Sifreni duzgun qeyd edin");
+            }
+        }
+    }
+}
